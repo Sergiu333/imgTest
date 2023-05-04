2975951951cc44f87eb10054b4167e4f27e30fed
Anghelenici Sergiu <sergiu.anghelenici@corlab.tech>
2023-05-04 16:08:52 +0300

    push

--- src/api/produs/routes/produs.js
+++ src/api/produs/routes/produs.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test router
+ * produs router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::test.test');
+module.exports = createCoreRouter('api::produs.produs');
